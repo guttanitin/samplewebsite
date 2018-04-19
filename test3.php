883122686b3e89c1b9074865e42367f8f7eb11f8
Nitin Gutta <ngutta@umich.edu>
2018-04-19 08:10:04 -0400

    update data

--- test3.php
+++ test3.php
@@ -1,5 +1,5 @@
 <?php
-
+$prices[]= {1,2,3,4,5,6,7,8,9,10,11,12};
 ?>
 <html>
     <script src="https://code.highcharts.com/highcharts.js"></script>
@@ -57,7 +57,7 @@ Highcharts.chart('container', {
     },
     series: [{
         name: 'Tokyo',
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        data: <?php echo $prices; ?>
 
     }]
 });
