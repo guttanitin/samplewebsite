0e7a9177c66e73d0a3acf8e8efb02d6d88caa748
Nitin Gutta <ngutta@umich.edu>
2018-04-20 15:44:43 -0400

    change iframe

--- dashboard.php
+++ dashboard.php
@@ -293,7 +293,7 @@ $nvdpercchange = round(($nvdchangeprice / $nvdopenprices[0]) * 100.0,2);
                     </table>
                 </div>
                 <div class ="col-sm-7">
-                    <iframe src="https://nitintest.azurewebsites.net/test2.php" width="600" height="400" scrolling="no" style="padding-left: 25px; overflow:hidden; margin-top:0px; margin-left:0px; border:none;" frameBorder="0"></iframe>
+                    <iframe src="https://nitintest.azurewebsites.net/test2.php" width="100%" height="100%" scrolling="no" style="padding-left: 25px; overflow:hidden; margin-top:0px; margin-left:0px; border:none;" frameBorder="0"></iframe>
                 </div>
             
             </div>
